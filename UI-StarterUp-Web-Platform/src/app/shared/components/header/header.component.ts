a2542692927c0d63f2b11c5a3f6c761e58e32cdb
mirelaverebceanu <verebceanumirela@gmail.com>
2021-03-17 20:04:40 +0200

    add header

---
+++ UI-StarterUp-Web-Platform/src/app/shared/components/header/header.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'src/app/menu-item';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent implements OnInit {
+
+  menuItems: MenuItem[] = [
+    {
+      label: 'Sign Up',
+      icon: 'login',
+      showOnMobile: true,
+      showOnTablet: true,
+      showOnDesktop: true
+    },
+    {
+      label: 'About',
+      icon: 'help',
+      showOnMobile: false,
+      showOnTablet: true,
+      showOnDesktop: true
+    },
+    {
+      label: 'Pricing',
+      icon: 'attach_money',
+      showOnMobile: false,
+      showOnTablet: false,
+      showOnDesktop: true
+    },
+    {
+      label: 'Docs',
+      icon: 'notes',
+      showOnMobile: false,
+      showOnTablet: true,
+      showOnDesktop: true
+    },
+    {
+      label: 'Showcase',
+      icon: 'slideshow',
+      showOnMobile: false,
+      showOnTablet: false,
+      showOnDesktop: true
+    },
+    {
+      label: 'Blog',
+      icon: 'rss_feed',
+      showOnMobile: false,
+      showOnTablet: false,
+      showOnDesktop: true
+    },
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ UI-StarterUp-Web-Platform/src/app/shared/shared.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule} from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
+
+@NgModule({
+  declarations: [HeaderComponent, FooterComponent],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatMenuModule
+  ],
+  exports: [
+    FooterComponent,
+    HeaderComponent
+  ]
+})
+export class SharedModule { }
