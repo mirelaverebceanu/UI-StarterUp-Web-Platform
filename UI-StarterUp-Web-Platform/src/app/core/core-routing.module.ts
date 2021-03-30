24e34730140faeb8aa6dfae65774cda2cbc86912
mirelaverebceanu <verebceanumirela@gmail.com>
2021-03-30 23:25:36 +0300

    add home route

--- UI-StarterUp-Web-Platform/src/app/core/core-routing.module.ts
+++ UI-StarterUp-Web-Platform/src/app/core/core-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from '../auth/components/login/login.component';
 import { RegisterComponent } from '../auth/components/register/register.component';
+import { HomeComponent } from '../home/components/home/home.component';
 
 const routes: Routes = [
+  { path: '/', component: HomeComponent},
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent}
 ];
