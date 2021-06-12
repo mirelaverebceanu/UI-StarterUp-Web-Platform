893d442f0086ea0dbcc6b70effa522061dd93086
mirelaverebceanu <verebceanumirela@gmail.com>
2021-06-13 01:22:09 +0300

    update token storage

--- UI-StarterUp-Web-Platform/src/app/tokenStorage.service.ts
+++ UI-StarterUp-Web-Platform/src/app/tokenStorage.service.ts
@@ -3,58 +3,6 @@ import { Injectable } from '@angular/core';
 import jwtDecode from 'jwt-decode';
 import { Observable } from 'rxjs';
  
-@Injectable()
-export class JWTTokenService {
- 
-    jwtToken!: string;
-    decodedToken!: { [key: string]: string; };
- 
-    constructor() {
-    }
- 
-    setToken(token: string) {
-      if (token) {
-        this.jwtToken = token;
-      }
-    }
- 
-    decodeToken() {
-      if (this.jwtToken) {
-      this.decodedToken = jwtDecode(this.jwtToken);
-      }
-    }
- 
-    getDecodeToken() {
-      return jwtDecode(this.jwtToken);
-    }
- 
-    getUser() {
-      this.decodeToken();
-      return this.decodedToken ? this.decodedToken.displayname : null;
-    }
- 
-    getEmailId() {
-      this.decodeToken();
-      return this.decodedToken ? this.decodedToken.email : null;
-    }
- 
-}
-
-@Injectable()
-export class LocalStorageService {
- 
-    set(key: string, value: string) {
-        localStorage.setItem(key, value);
-    }
- 
-    get(key: string) {
-        return localStorage.getItem(key);
-    }
- 
-    remove(key: string) {
-        localStorage.removeItem(key);
-    }
-}
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
@@ -74,10 +22,3 @@ export class AuthInterceptor implements HttpInterceptor {
   }
 
 }
-
-export class JwtResponse{
-    constructor(
-      public jwttoken:string,
-       ) {}
-  
-  }
